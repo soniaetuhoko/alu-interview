9a959b6f6482a3a94681bb4907b80efd4a6b9c1f
soniaetuhoko <s.etuhoko@alustudent.com>
2025-06-18 11:12:36 +0200

    Add pascal's Triangle solution

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row is always [1]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]  # Start with 1
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j - 1] + prev_row[j])  # Sum of two above
+        new_row.append(1)  # End with 1
+        triangle.append(new_row)
+
+    return triangle
